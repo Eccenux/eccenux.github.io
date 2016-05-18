dd6b6b678e7f39e1b07a1c6c252057e7c9a359ae
Eccenux <eccenux+git@gmail.com>
2016-05-18 04:58:17 +0200

    Export / import to storage

--- js/ExportTools.js
+++ js/ExportTools.js
@@ -23,6 +23,31 @@ ExportTools.prototype.hide = function() {
 	this.closeButton.style.display = 'none';
 	this.saveButton.style.display = 'none';
 };
+
+ExportTools.prototype.exportData = function() {
+	var _self = this;
+	localforage.getItem(this.storageKey).then(function(value) {
+		var json = '';
+		if (value === null) {
+			console.log('No data stored yet');
+		} else {
+			json = JSON.stringify(value);
+		}
+		_self.helperInput.value = json;
+	}).catch(function(err) {
+		console.error('Problem reading from storage!', err);
+	});
+};
+ExportTools.prototype.importData = function(onSuccess) {
+	var json = this.helperInput.value;
+	localforage.setItem(this.storageKey, JSON.parse(json)).then(function() {
+		console.log('Data import finished');
+		onSuccess();
+	}).catch(function(err) {
+		console.error('Problem saving data to storage!', err);
+	});
+};
+
 ExportTools.prototype.init = function() {
 	var _self = this;
 
@@ -35,6 +60,7 @@ ExportTools.prototype.init = function() {
 		_self.hide();
 		helperInput.style.display = '';
 		closeButton.style.display = '';
+		_self.exportData();
 	};
 	this.toolsContainer.appendChild(exportButton);
 
@@ -55,7 +81,9 @@ ExportTools.prototype.init = function() {
 	var saveButton = document.createElement('button');
 	saveButton.innerHTML = 'Save';
 	saveButton.onclick = function () {
-		_self.hide();
+		_self.importData(function(){
+			_self.hide();
+		});
 	};
 	saveButton.style.display = 'none';
 	this.toolsContainer.appendChild(saveButton);
