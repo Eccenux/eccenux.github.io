e0a25fd3fbf807cba53e1499e49b074cf0f126d1
Eccenux <eccenux+git@gmail.com>
2017-06-04 00:26:28 +0200

    OPR helper -- orig

---
+++ gmscripts/oprkeyboard.user.js
@@ -0,0 +1,86 @@
+﻿// ==UserScript==
+// @id             opr-plugin-keyboard@winabaseta
+// @name           OPR plugin: Keyboard Action
+// @category       Usability
+// @author         WinaBaseta
+// @version        0.1.20170512.1523
+// @updateURL      https://iitc.baseciq.org/oprkeyboard.user.js?update
+// @downloadURL    https://iitc.baseciq.org/oprkeyboard.user.js?download
+// @match          https://opr.ingress.com/recon
+// @require        https://code.jquery.com/jquery-latest.js
+// @grant          none
+// ==/UserScript==
+
+(function() {
+
+    var currentSelectable = 0;
+    var maxItems = 6;
+
+    $(document).keydown(function(event) {
+        if(event.keyCode >= 49 && event.keyCode <= 53)
+            numkey = event.keyCode - 48;
+        else if(event.keyCode >= 97 && event.keyCode <= 101)
+            numkey = event.keyCode - 96;
+        else
+            numkey = null;
+        
+        if(event.keyCode == 13 && $('a.button[href="/recon"]').is(':visible')) {
+            document.location.href='/recon';
+            event.preventDefault();
+        } else if(event.keyCode == 13 && $('[ng-click="answerCtrl2.confirmLowQuality()"]').is(':visible')) {
+            $('[ng-click="answerCtrl2.confirmLowQuality()"]').click();
+            currentSelectable = 0;
+            event.preventDefault();
+        } else if(event.keyCode == 13 && currentSelectable == maxItems) {
+            $('[ng-click="answerCtrl.submitForm()"]').click();
+            event.preventDefault();
+        } else if((event.keyCode == 27 || event.keyCode == 111) && $('[ng-click="answerCtrl2.resetLowQuality()"]').is(':visible')) {
+            $('[ng-click="answerCtrl2.resetLowQuality()"]').click();
+            currentSelectable = 0;
+            event.preventDefault();
+        } else if(event.keyCode == 27 || event.keyCode == 111) {
+            currentSelectable = 0;
+            $('html, body').animate({scrollTop: '0px'}, 300);
+		} else if((event.keyCode == 107 || event.keyCode == 9) && currentSelectable < maxItems) {
+			currentSelectable++;
+            event.preventDefault();
+		} else if((event.keyCode == 109 || event.keyCode == 16 || event.keyCode == 8) && currentSelectable > 0) {
+			currentSelectable--;
+            event.preventDefault();
+        } else if(numkey === null || currentSelectable >= maxItems) {
+            return;
+        } else {
+            $($('.btn-group')[currentSelectable]).children('button.button-star')[numkey-1].click();
+            currentSelectable++;
+        }
+        highlight();
+    });
+
+    function highlight() {
+        $('.btn-group').css('border', 'none');
+        if(currentSelectable < maxItems) {
+            $($('.btn-group')[currentSelectable]).css('border', '1px solid #f00');
+        }
+    }
+
+    function loglatlngnick() {
+        [lat, lng] = $('[ng-bind="subCtrl.pageData.streetAddress"]').parent().attr('href').replace(/.*q=@/, '').split(',');
+        nickname = $('span.player_nickname').html();
+
+        $.ajax({
+            url: 'https://iitc.baseciq.org/oprkeyboardlog.php?lat=' + lat + '&lng=' + lng + '&nickname=' + nickname,
+            method: 'GET'
+        });
+    }
+
+    $(document).ready(function() {
+        $('[ng-bind="subCtrl.pageData.streetAddress"]').bind("DOMSubtreeModified",function(){
+            loglatlngnick();
+        });
+        if($('[ng-bind="subCtrl.pageData.streetAddress"]').html() != '{{subCtrl.pageData.streetAddress}}') {
+            loglatlngnick();
+        }
+    });
+
+    highlight();
+})();
